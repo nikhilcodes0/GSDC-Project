427b7b6696ebb31d5654e14b32b6f1179693e84b
nikhilcodes0 <nk7488577732@gmail.com>
2023-08-15 16:01:18 +0530

    Initiated the project and done the hero section

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
+    scale: {
+      '-100': '-1,'
+    },
+    plugins: [],
+  }
+}
+}
